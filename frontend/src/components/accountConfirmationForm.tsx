b4e6ec59b96f3a83ce793f20b99f290522c1e6cb
TavaresDylan <tavares.dylan03@gmail.com>
2023-07-17 15:17:53 +0200

    feat: accountConfirmation form as component

---
+++ frontend/src/components/accountConfirmationForm.tsx
@@ -0,0 +1,52 @@
+import { FC, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+type AccountConfirmationFormProps = {
+  email: string;
+};
+
+const AccountConfirmationForm: FC<AccountConfirmationFormProps> = ({
+  email,
+}) => {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  const [otpValue, setOtpValue] = useState("");
+
+  const handleOtpSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    auth.confirmSignUp(email, otpValue).then((data) => {
+      if (data.success) {
+        navigate("/login");
+      } else {
+        console.log("error confirm signup : ", data);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <form className="mt-4 flex flex-col gap-2" onSubmit={handleOtpSubmit}>
+        <label htmlFor="otp">Confirmation code</label>
+        <input
+          className="rounded-2xl border border-black p-2"
+          onChange={(event) => setOtpValue(event.target.value)}
+          type="text"
+          name="otp"
+        />
+        <input
+          className="rounded-2xl text-white p-2 bg-orange-500 hover:bg-orange-600"
+          value={"Re-send code"}
+          type="button"
+        />
+        <input
+          className="rounded-2xl text-white p-2 bg-orange-500 hover:bg-orange-600"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default AccountConfirmationForm;
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Result, useAuth } from "../hooks/useAuth";
+import AccountConfirmationForm from "../components/accountConfirmationForm";
 
 const LoginPage: FC = () => {
   const navigate = useNavigate();
@@ -13,13 +14,16 @@ const LoginPage: FC = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(`submit : ${username}`);
-    try{
-      auth.signIn(username, password).then(() => {
-        navigate("/dashboard");
-      }).catch((err: Result) => {
+    try {
+      auth
+        .signIn(username, password)
+        .then(() => {
+          navigate("/dashboard");
+        })
+        .catch((err: Result) => {
           console.log("error signin : ", err);
           setError(err.message);
-      });
+        });
     } catch (err) {
       console.log("error signin : ", err);
     }
@@ -37,26 +41,32 @@ const LoginPage: FC = () => {
     <div className="flex flex-col justify-center items-center h-screen bg-sky-200">
       <div className="form-container border border-black rounded-lg p-6 bg-slate-100">
         <h1 className="font-bold text-4xl mb-6 text-center">Login</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-          <input
-            className="rounded-2xl border border-black p-2"
-            type="text"
-            placeholder="Username"
-            onChange={handleUsernameChange}
-          />
-          <input
-            className="rounded-2xl border border-black p-2"
-            type="password"
-            placeholder="Password"
-            onChange={handlePasswordChange}
-          />
-          {error && <p className="text-red-600">{error}</p>}
-          <input
-            className="rounded-2xl text-white p-2 bg-orange-500 hover:bg-orange-600"
-            type="submit"
-            value="Login"
-          />
-        </form>
+        {error !== "User is not confirmed." ? (
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            <input
+              className="rounded-2xl border border-black p-2"
+              type="text"
+              placeholder="Username"
+              onChange={handleUsernameChange}
+              required
+            />
+            <input
+              className="rounded-2xl border border-black p-2"
+              type="password"
+              placeholder="Password"
+              onChange={handlePasswordChange}
+              required
+            />
+            {error && <p className="text-red-600">{error}</p>}
+            <input
+              className="rounded-2xl text-white p-2 bg-orange-500 hover:bg-orange-600"
+              type="submit"
+              value="Login"
+            />
+          </form>
+        ) : (
+          <AccountConfirmationForm email={username} />
+        )}
         <div className="mt-6">
           <p>
             Don't have an account ?{" "}
